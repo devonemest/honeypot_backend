9df319e6da4c94333518caeec6a6c65d590f23cd
devonemest <48983182+devonemest@users.noreply.github.com>
2020-05-11 21:51:18 +0300

    работающая часть
    код без контроллеров и роутинга!!!!!!!!!

--- server/app/Modules/exportdb.js
+++ server/app/Modules/exportdb.js
@@ -3,9 +3,7 @@ module.exports = function(data,cp){
 for(var i=1; i <= Object.keys(data.text).length ; i++){
     text +=data.text[i] + " ";
 }
-var fcmid = "12";
-var yid = "123";
-cp.query(`INSERT INTO data VALUES(${data.hash[1]},${fcmid},${yid},${data.geo[1]},${data.geo[2]},'${text}');`, function(err, rows, fields) {
+cp.query(`INSERT INTO data VALUES(${data.hash[1]},${data.fcmid[1]},${data.yid[1]},${data.geo[1]},${data.geo[2]},'${text}');`, function(err, rows, fields) {
     if (err) throw err;
 });
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const config = require('./config.json');// подключение конфига через require
-const mysql = require('mysql');
-// const data = require('../src/app/service/data.service.spec.ts') экспорт обьекта data из файла
+const config         = require('./app/config/config.json');// подключение конфига через require
+const mysql          = require('mysql');
+const express        = require('express');
+const app            = express();
 
 const cp = mysql.createConnection({
     host     : config.ipdb,
@@ -18,28 +19,27 @@ var obj = {
         2: "12.342142144"
     } ,
     text:{
-        1:"Я сделал",
-        2:"этот код",
-        3:"работающим",
-        4:""
+        1:"Примите",
+        2:"Пожалуйсто",
+        3:"курсовую",
+        4:"Спасите"
     } ,
     hash:{
         1: "1234567123"
+    },
+    fcmid:{
+        1:"12345"
+    },
+    yid:{
+        1:"1234234"
     }
 }
-// определяем обработчик для маршрута "/"
-// server.js
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const app            = express();
 app.get("/", function(request, response){
-
     // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+    response.send("<h2>Это корень Backend!</h2>");
 });
 
-app.get("/data", function(request, response){
-
+app.post("/data", function(request, response){
     //функция которая записывает данные в бd
     const data = require('./Modules/exportdb.js')
     data(obj,cp)
@@ -48,54 +48,8 @@ app.get("/data", function(request, response){
 
 // server.js
 const port = 8000;
-require('./app/routes')(app, {});
 app.listen(port, () => {
     console.log('We are live on ' + port);
 });
-
-
-
-// dotenv.config();
-//
-// // config passport and connect to DB
-// require('./config/passport')(passport);
-//
-// // set up express
-// app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(fileUpload());
-//
-//
-// // config passport
-// app.use(session({
-//     secret: config.sessionSecret,
-//     resave: true,
-//     saveUninitialized: false
-// } ));
-// // session secret
-// app.use(passport.initialize());
-// app.use(passport.session()); // persistent login sessions
-// app.use(flash());
-// app.use(function(err, req, res, next) {
-//     if (err) {
-//         req.logout();
-//         next();
-//     } else {
-//         next();
-//     }
-// });
-
-
-// var http = require('http');
-// // Create server
-// http = http.createServer();
-//
-//
-// http.listen(config.port, function(){
-//     console.log('Server listening on port ' + config.port);
-// });
-
 
 
